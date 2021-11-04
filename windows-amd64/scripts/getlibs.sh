8fc7ee465e50309b8ce5f90f9e7ef4010a1b603f
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2021-11-04 02:00:23 +0100

    windows: updated library name in extraction scripts

--- windows-amd64/scripts/getlibs.sh
+++ windows-amd64/scripts/getlibs.sh
@@ -10,7 +10,7 @@ set -ex
 cp $LIBPATH/libogg-0.dll $DISTPATH
 cp $LIBPATH/libopus-0.dll $DISTPATH
 cp $LIBPATH/libopusfile-0.dll $DISTPATH
-cp $LIBPATH/libportaudio-2.dll $DISTPATH
+cp $LIBPATH/libportaudio.dll $DISTPATH
 cp $LIBPATH/libsamplerate-0.dll $DISTPATH
 cp $LIBPATH/libgcc_s_seh-1.dll $DISTPATH
 cp $LIBPATH/libwinpthread-1.dll $DISTPATH
